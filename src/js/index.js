e93f54c88ae04555007714d8b5530476f48c5c6a
a90100 <snh90100@gmail.com>
2023-04-11 20:42:46 +0800

    feat: finish option and popup page ui

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,19 @@
 
 const checkingUrl = 'https://www.udemy.com';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.disable();
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.action.disable();
+// });
 
 // onActivated: 瀏覽器 tab 切換時觸發
 // 透過 chrome.tabs.get 和 tabId 去取得該 tab 的詳細資訊
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  chrome.tabs.get(activeInfo.tabId, (tab) => {
-    const isLoadingUdemyWebsite = tab.status === 'loading';
-    if (tab.url.includes(checkingUrl) || (isLoadingUdemyWebsite && tab.pendingUrl.includes(checkingUrl))) {
-      chrome.action.enable();
-    } else {
-      chrome.action.disable();
-    }
-  });
-});
+// chrome.tabs.onActivated.addListener((activeInfo) => {
+//   chrome.tabs.get(activeInfo.tabId, (tab) => {
+//     const isLoadingUdemyWebsite = tab.status === 'loading';
+//     if (tab.url.includes(checkingUrl) || (isLoadingUdemyWebsite && tab.pendingUrl.includes(checkingUrl))) {
+//       chrome.action.enable();
+//     } else {
+//       chrome.action.disable();
+//     }
+//   });
+// });
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
+import '../css/popup.scss';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const slider = document.getElementById("slider");
+  const output = document.getElementById("output");
+  output.innerHTML = slider.value;
+  
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+  // 這裡只需要作呈現數字就好，監聽滑動由 injector.js 處理
 });
